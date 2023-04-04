7a1d4aa9ed9b54598c7a59aa8295b1b1e0a1dfec
Irina Vergun <antohka803@gmail.com>
2023-04-04 17:49:05 +0300

    Стиль кода

--- script.js
+++ script.js
@@ -1,160 +1,117 @@
 function runClock(){
-    const cechet=()=>{
+    const tick=()=>{
         let now =new Date()
         let options={
             hour:'numeric',
             minute:'numeric',
             second:'numeric',
         }
-        let vremia=document.getElementById('vremia') 
-        if(vremia!=null){
-            vremia.innerHTML=now.toLocaleString('ru',options)
+        let clock=document.getElementById('clock') 
+        if(clock!=null){
+            clock.innerHTML=now.toLocaleString('ru',options)
         }};
-        cechet()
-        setInterval(cechet,1000)
+        tick()
+        setInterval(tick,1000)
 }
 
-
-const button=document.querySelector('.btn-info')
+const button = document.querySelector('.btn-info')
 // получаем значение импута
 function handleButtonClick(){
-    const inputVal=document.getElementById('exampleInputEmail1').value;
-    if (inputVal==''){
+    const inputVal=document.getElementById('cityInput').value;
+    if (inputVal === '') {
         alert('Ошибка: введите город в строку поиска')
-    
-    }
-    else {
+    } else {
         console.log(inputVal)
         fetchWeather(inputVal) 
     }
-    
-    
 }
-button.onclick=handleButtonClick;
-lol()
+button.onclick = handleButtonClick;
 
-function lol(){
-     let formmm=document.getElementById('formm')
-formmm.onsubmit=(event)=>{
-    event.preventDefault()
-}}
-
-
+const form = document.getElementById('form')
+form.onsubmit = (event) => {
+    event.preventDefault();
+}
 
 // функция запрашивает данные о погоде 
 async function fetchWeather(city){
-    const url=`https://api.openweathermap.org/data/2.5/weather?q=${city}&lang=ru&units=metric&APPID=483f7cc145c4efc5f7411a867744ed88`
+    const url=`https://api.openweathermap.org/data/2.5/weather?q=${city}&lang=ru&units=metric&APPID=483f7cc145c4efc5f7411a867744ed88`;
     try {
-        const response=await fetch(url)
+        const response = await fetch(url);
 
         if (response.ok) { 
-  const data=await response.json()
-        console.log('Data', data)
+            const data=await response.json()
+            console.log('Data', data)
         
-        serverdal(data)
-        kartinki(data)
+            showData(data);
+            showImages(data);
         
-      let container=document.getElementById('cont')
-        container.style.visibility = 'visible';
-        // contrrr(data)
-            
-          } else if(response.status===404){
+            const container = document.getElementById('container');
+            container.style.visibility = 'visible';            
+          } else if (response.status===404) {
             alert("Введите правильно город или страну. Ошибка: " + response.status);
           }
         else {
             alert('HTTP: ' + response.status)
         }
-
-      
-        //  показать блок
     } catch(e){
         console.error(e)
     }
-    
 }
 
-// function contrrr(){
-// let blocc=document.getElementById('cont')
-//       if(blocc.onclick()){
-        
-//       }
-// }
-
-
-function serverdal(data){
-    let temp=document.getElementById('temperature')
-    if(temp!=null){
-        temp.innerHTML=data.main.temp
+function showData(data) {
+    const temp = document.getElementById('temperature');
+    if(temp !== null){
+        temp.innerHTML = data.main.temp;
+    }
     
-        let vlag=document.getElementById('vlajnost')
-        if(vlag!=null){
-            vlag.innerHTML=data.main.humidity
-        }
-     let inmoment=document.getElementById('namoment')
-     if(inmoment!=null){
+    const vlag = document.getElementById('vlajnost');
+    if(vlag !== null){
+        vlag.innerHTML=data.main.humidity
+    }
+    
+    const inmoment = document.getElementById('namoment');
+    if(inmoment !== null) {
         inmoment.innerHTML=data.weather[0].description
-
-     }   
-     let ochuch=document.getElementById('ochuchenie')
-     if(ochuch!=null){
-        ochuch.innerHTML=data.main.feels_like
-     }
-     let veter=document.getElementById('skorost')
-     if(veter!=null){
-        veter.innerHTML=data.wind.speed
-     }
-
-    
     }
-    // img.src="images/как ощущается температура.png"
+
+    const ochuch = document.getElementById('ochuchenie');
+    if(ochuch !== null){
+        ochuch.innerHTML=data.main.feels_like
+    }
+
+    const veter = document.getElementById('skorost');
+    if(veter !== null) {
+        veter.innerHTML=data.wind.speed
+    }
 }
 
-function kartinki(data){
-    const img=document.getElementById('meniai');
+function showImages(data){
+    const img = document.getElementById('meniai');
 
-    if(data.weather[0].id >= 200 && data.weather[0].id < 600){
-        // console.log('дождь')
+    if(data.weather[0].id >= 200 && data.weather[0].id < 600) {
         img.src='images/дождь.png'
-    }
-    else if(data.weather[0].id >= 802 && data.weather[0].id < 900){
-    console.log('облочно')
-    img.src='images/облочно.png'
-    }
-    else if(data.weather[0].id === 801){
-        console.log('переменная облочность')
+    } else if(data.weather[0].id >= 802 && data.weather[0].id < 900){
+        img.src='images/облочно.png';
+    } else if(data.weather[0].id === 801) {
         img.src='images/переменая облочнось.png'
-    }
-    else if( data.weather[0].id >= 600 && data.weather[0].id < 700){
-    console.log('снег')
-    img.src='images/снег.png'
-    }
-    else if( data.weather[0].id === 800){
-        console.log('солнечно')
+    } else if( data.weather[0].id >= 600 && data.weather[0].id < 700) {
+        img.src='images/снег.png'
+    } else if( data.weather[0].id === 800) {
         img.src="images/солнечно.png"
-    }
-    else{
-        console.log('все остальное')
+    } else {
         img.src='images/погода по умолчанию.png'
     }
 
+    // data.weather[0].id - погодные условия
 
-
+    // дождь: data.weather[0].id >= 200 && data.weather[0].id < 600 
+    // облачно: data.weather[0].id >= 802 && data.weather[0].id < 900
+    // перем. облачность: data.weather[0].id === 801
     
-
-        // data.weather[0].id - погодные условия
-
-        // дождь: data.weather[0].id >= 200 && data.weather[0].id < 600 
-        // облачно: data.weather[0].id >= 802 && data.weather[0].id < 900
-        // перем. облачность: data.weather[0].id === 801
-        
-        // снег: data.weather[0].id >= 600 && data.weather[0].id < 700
-        // солнечно: data.weather[0].id === 800
-        // погода по умолчанию: все остальное   
-
-        // img.src = 'путь к картинке';
+    // снег: data.weather[0].id >= 600 && data.weather[0].id < 700
+    // солнечно: data.weather[0].id === 800
+    // погода по умолчанию: все остальное   
 }
-      
-
 
 runClock()
 
